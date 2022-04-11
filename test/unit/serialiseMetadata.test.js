f6021c1bd10310c155aa14e7483408cf3bfdd844
chronoDave <davidwolters6758@gmail.com>
2022-04-11 21:51:33 +0200

    Updated tests

---
+++ test/unit/serialiseMetadata.test.js
@@ -0,0 +1,31 @@
+const test = require('tape');
+
+const createConfig = require('../../src/createConfig');
+const serialiseMetadata = require('../../src/serialiseMetadata');
+
+const testMetadataFormat = (script, expected) => t => {
+  const name = 'output';
+
+  const config = createConfig({
+    files: ['createConfig.js', 'run.js'],
+    metadata: { script },
+    output: { path: __dirname, name }
+  });
+
+  t.true(serialiseMetadata(config).includes(expected));
+
+  t.end();
+};
+
+[
+  { lang: 'javascript', script: 'js', expected: '// script:' },
+  { lang: 'lua', script: 'lua', expected: '-- script:' },
+  { lang: 'moon', script: 'moon', expected: '-- script:' },
+  { lang: 'fennel', script: 'fennel', expected: ';; script:' },
+  { lang: 'ruby', script: 'ruby', expected: '# script:' },
+  { lang: 'wren', script: 'wren', expected: '// script:' },
+  { lang: 'squirrel', script: 'squirrel', expected: '// script:' }
+].forEach(({ lang, script, expected }) => test(
+  `[serialiseMetadata] formats metadata for ${lang} when 'script' is '${script}'`,
+  testMetadataFormat(script, expected)
+));
